f9e84afbd2ede48c24cd3ead46a5be7fb7bedb27
jtkiesel <rstudio@example.com>
2017-02-18 13:32:59 -0500

    Added gyro test.

---
+++ test/gyroTest.c
@@ -0,0 +1,21 @@
+#include "../gyro/gyro.c"
+
+Gyro gyro;
+
+task background() {
+	while (true) {
+		update(&gyro);
+	}
+}
+
+task main() {
+	newGyro(&gyro, in1);
+
+	startTask(background);
+
+	while (getAngle(&gyro) < 90.0) {
+		motor[port2] = 127;
+		motor[port3] = -127;
+	}
+	motor[port2] = motor[port3] = 0;
+}
