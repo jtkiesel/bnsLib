a3d2ee40b8e7711c96fa3a7f6f0b7e038474d990
jtkiesel <rstudio@example.com>
2017-02-18 13:53:23 -0500

    Added calibration and comments to gyroTest.

--- test/gyroTest.c
+++ test/gyroTest.c
@@ -11,10 +11,12 @@ task background() {
 }
 
 task main() {
-	newGyro(&gyro, in1);
+	newGyro(&gyro, in1);  // Set up gyro in analog port 1.
+	calibrate(&gyro, 1000, 1);  // Calibrate gyro.
 
-	startTask(background);
+	startTask(background);  // Update gyro angle in the background.
 
+	// Turn left 90 degrees.
 	while (getAngle(&gyro) < 90.0) {
 		motor[port2] = 127;
 		motor[port3] = -127;
